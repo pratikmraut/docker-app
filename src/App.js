7fbec396c6c25089d182414eeba8f301956ebb30
pratik <pratikmraut123@gmail.com>
2023-01-12 10:56:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pratik Raut Sangli
         </p>
         <a
           className="App-link"
